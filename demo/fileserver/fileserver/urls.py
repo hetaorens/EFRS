d824446d7b76f5830b8064acd0e27a42bc11def5
hetaorens <1656798028@qq.com>
2023-06-05 11:29:05 +0800

    0.0.1 demo

--- demo/fileserver/fileserver/urls.py
+++ demo/fileserver/fileserver/urls.py
@@ -1,9 +1,23 @@
+# from django.contrib import admin
+# from django.urls import path, include
+from fileupload import views as fileupload_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', fileupload_views.home, name='home'),
+    
+# ]
+
+
+# urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from fileupload import views as fileupload_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', fileupload_views.home, name='home'),
     path('fileupload/', include('fileupload.urls', namespace='fileupload')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
